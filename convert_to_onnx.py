19f4501b3d8c760bf7240d2bbd0047e54fcc605f
Konstantinos Vasios <konstantinos.vasios@iccs.gr>
2026-01-20 15:53:50 +0200

    converting to onnx output directly at model dir

--- convert_to_onnx.py
+++ convert_to_onnx.py
@@ -2,13 +2,18 @@ import torch
 import numpy as np
 
 from nets import Model
+from pathlib import Path
 
 if __name__ == '__main__':
 
-	model_path = "models/crestereo_eth3d.pth"
+	this_dir = Path(__file__).resolve().parent
+	models_dir = this_dir / "models"
+	models_dir.mkdir(parents=True, exist_ok=True)
+
+	model_path = models_dir / "crestereo_eth3d.pth"
 
 	model = Model(max_disp=256, mixed_precision=False, test_mode=True)
-	model.load_state_dict(torch.load(model_path), strict=True)
+	model.load_state_dict(torch.load(str(model_path)), strict=True)
 	model.eval()
 
 	in_h, in_w = (480, 640)
@@ -22,7 +27,7 @@ if __name__ == '__main__':
 	# Export the model
 	torch.onnx.export(model,               
 	                  (t1, t2, flow_init),
-	                  "crestereo.onnx",   # where to save the model (can be a file or file-like object)
+	                  str(models_dir / "crestereo.onnx"),   # where to save the model (can be a file or file-like object)
 	                  export_params=True,        # store the trained parameter weights inside the model file
 	                  opset_version=12,          # the ONNX version to export the model to
 	                  do_constant_folding=True,  # whether to execute constant folding for optimization
@@ -34,7 +39,7 @@ if __name__ == '__main__':
 	# Ref: https://github.com/pytorch/pytorch/pull/73760
 	torch.onnx.export(model,               
 	                  (t1_half, t2_half),
-	                  "crestereo_without_flow.onnx",   # where to save the model (can be a file or file-like object)
+	                  str(models_dir / "crestereo_without_flow.onnx"),   # where to save the model (can be a file or file-like object)
 	                  export_params=True,        # store the trained parameter weights inside the model file
 	                  opset_version=12,          # the ONNX version to export the model to
 	                  do_constant_folding=True,  # whether to execute constant folding for optimization
